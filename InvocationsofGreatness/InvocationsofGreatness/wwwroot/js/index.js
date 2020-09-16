afeac27887a4f9a765637052bfa9e0ce85ee7ec9
FramesJanko <rheckman4143@gmail.com>
2020-09-15 23:17:34 -0400

    attempt to publish

---
+++ InvocationsofGreatness/InvocationsofGreatness/wwwroot/js/index.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
+
+    console.log("Hello World");
+
+    var startingMenu = $(".startingMenu li");
+    startingMenu.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+var $loginToggle = $("#loginToggle");
+var $popupForm = $(".popupForm");
+$loginToggle.on("click", function () {
+    $popupForm.toggle();
+});
+});
